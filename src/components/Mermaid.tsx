d686aefd58be0d61125cfae3f9771089c2db6c03
Ilya Chernichkin <incwadra@gmail.com>
2025-02-20 20:43:14 +0500

    chore(dev): dividing functionality into hooks and final code #1

--- src/components/Mermaid.tsx
+++ src/components/Mermaid.tsx
@@ -1,33 +1,6 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
-import type { MermaidConfig, RenderResult } from 'mermaid'
-import mermaid from 'mermaid'
-import SvgPanZoom from 'svg-pan-zoom'
-
+import React, { useContext, useEffect, useMemo } from 'react'
 import MermaidControl from './MermaidControl'
-import { useDarkMode } from '../utils'
-
-const renderDiagram = async (
-  id: string,
-  code: string,
-  printMode: boolean
-): Promise<RenderResult> => {
-  mermaid.initialize({
-    startOnLoad: false,
-    theme: printMode ? 'default' : inkdrop.config.get('mermaid.theme'),
-    themeCSS: inkdrop.config.get('mermaid.themeCSS'),
-    themeVariables: JSON.parse(
-      inkdrop.config.get<string>('mermaid.themeVariables') || '{}'
-    )
-  })
-  try {
-    return await mermaid.render(id, code)
-  } catch (err) {
-    if (err instanceof Error) {
-      throw err
-    }
-    throw new Error('Unknown error')
-  }
-}
+import { useDarkMode, useInkdropConfig, useMermaidRendering } from '../utils'
 
 const Mermaid: React.FC<CodeComponentProps> = ({ children }) => {
   const id = useMemo(
@@ -39,175 +12,47 @@ const Mermaid: React.FC<CodeComponentProps> = ({ children }) => {
     []
   )
 
-  const observerTheme = useRef<Disposable | null>(null)
-  const observerPanZoom = useRef<Disposable | null>(null)
-  const observerUI = useRef<Disposable | null>(null)
-  const containerRef = useRef<HTMLDivElement | null>(null)
-  const parentRef = useRef<HTMLDivElement | null>(null)
+  const code = useMemo(() => {
+    if (typeof children === 'string') return children
+    if (Array.isArray(children) && typeof children[0] === 'string')
+      return children[0]
+    return ''
+  }, [children])
 
-  const [theme, setTheme] = useState<MermaidConfig['theme']>(
-    inkdrop.config.get('mermaid.theme') || 'default'
-  )
-  const [additionalUI, setAdditionalUI] = useState<boolean>(
-    inkdrop.config.get('mermaid.additionalUI')
-  )
-  const [type, setType] = useState<string>('')
+  const { theme, panZoomMode, additionalUI } = useInkdropConfig()
+
   const { printMode } = useContext(inkdrop.markdownRenderer.Context)
-  const [panZoomMode, setPanZoomMode] = useState<boolean>(
-    inkdrop.config.get('mermaid.panZoom')
-  )
-  const [panZoomInstance, setPanZoomInstance] = useState<ReturnType<
-    typeof SvgPanZoom
-  > | null>(null)
-  const [error, setError] = useState<Error | null>(null)
-
   const isDarkMode = useDarkMode()
 
-  useEffect(() => {
-    observerTheme.current = inkdrop.config.observe<MermaidConfig['theme']>(
-      'mermaid.theme',
-      theme => setTheme(theme)
-    )
-
-    observerPanZoom.current = inkdrop.config.observe<boolean>(
-      'mermaid.panZoom',
-      panZoom => setPanZoomMode(panZoom)
-    )
-
-    observerUI.current = inkdrop.config.observe<boolean>(
-      'mermaid.additionalUI',
-      additionalUI => setAdditionalUI(additionalUI)
-    )
-
-    return () => {
-      observerTheme.current?.dispose()
-      observerPanZoom.current?.dispose()
-      observerUI.current?.dispose()
-    }
-  }, [])
+  const { error, diagramType, containerRef, parentRef, panZoomInstance } =
+    useMermaidRendering(id, code, printMode, panZoomMode, additionalUI, theme)
 
   useEffect(() => {
     if (!parentRef.current) return
-
-    const preElement = parentRef.current?.parentElement
-    if (!preElement) return
-
-    if (isDarkMode && !printMode) {
-      preElement.setAttribute('data-mermaid-theme', 'dark')
-    } else if (!isDarkMode && !printMode) {
-      preElement.setAttribute('data-mermaid-theme', 'light')
-    }
+    const preElement = parentRef.current.parentElement
+    if (!preElement && !printMode) return
+    preElement?.setAttribute(
+      'data-mermaid-theme',
+      isDarkMode ? 'dark' : 'light'
+    )
   }, [isDarkMode, printMode])
 
   useEffect(() => {
-    if (!parentRef.current) return
-
-    parentRef.current.style.paddingTop =
-      additionalUI && !printMode ? '40px' : '0'
-  }, [additionalUI])
-
-  useEffect(() => {
-    const code =
-      typeof children === 'string'
-        ? children
-        : Array.isArray(children) && typeof children[0] === 'string'
-          ? children[0]
-          : ''
-
-    if (!code || !containerRef.current) return
-
-    let cancelled = false
-    let resizeObserver: ResizeObserver | null = null
-
-    const container = containerRef.current
-    const parrent = parentRef.current
-    renderDiagram(id, code, printMode)
-      .then(({ svg, diagramType, bindFunctions }) => {
-        if (cancelled || !svg.length) return
-
-        container.innerHTML = svg
-        const diagram = container.querySelector<SVGSVGElement>(`#${id}`)
-        if (!diagram) return
-
-        const viewBox = diagram.getAttribute('viewBox')
-        if (!viewBox) return
-
-        const [, , vbWidth, vbHeight] = viewBox
-          .trim()
-          .split(/[\s,]+/)
-          .map(parseFloat)
-
-        if ([vbWidth, vbHeight].some(isNaN)) return
-
-        diagram.setAttribute('height', '100%')
-        diagram.style.display = 'block'
-        diagram.style.maxWidth = '100%'
-
-        bindFunctions?.(container)
-
-        const recalcDimensions = () => {
-          const parentWidth = parrent?.clientWidth || container.clientWidth
-          container.style.width = `${parentWidth}px`
-          container.style.height = `${Math.ceil(parentWidth * (vbHeight / vbWidth))}px`
-        }
-
-        recalcDimensions()
-        resizeObserver = new ResizeObserver(recalcDimensions)
-        resizeObserver.observe(container.parentElement!)
-
-        if (panZoomMode && !printMode && additionalUI) {
-          setPanZoomInstance(
-            SvgPanZoom(diagram, {
-              zoomEnabled: true,
-              mouseWheelZoomEnabled: false
-            })
-          )
-        }
-
-        setType(diagramType)
-        setError(null)
-      })
-      .catch(err => !cancelled && setError(err))
-
-    return () => {
-      cancelled = true
-      resizeObserver?.disconnect()
+    if (parentRef.current) {
+      parentRef.current.style.paddingTop =
+        additionalUI && !printMode ? '40px' : '0'
     }
-  }, [children, theme, printMode, panZoomMode, additionalUI])
+  }, [additionalUI, printMode])
 
-  useEffect(() => {
-    if (!panZoomInstance || !containerRef.current) return
-
-    const resizeObserver = new ResizeObserver(() => {
-      panZoomInstance.resize()
-      panZoomInstance.center()
-      panZoomInstance.fit()
-    })
-
-    resizeObserver.observe(containerRef.current)
-
-    return () => {
-      resizeObserver.disconnect()
-    }
-  }, [panZoomInstance])
-
-  const handleZoomIn = () => {
-    panZoomInstance?.zoomIn()
-  }
-
-  const handleZoomOut = () => {
-    panZoomInstance?.zoomOut()
-  }
-
-  const handleReset = () => {
-    panZoomInstance?.reset()
-  }
+  const handleZoomIn = () => panZoomInstance?.zoomIn()
+  const handleZoomOut = () => panZoomInstance?.zoomOut()
+  const handleReset = () => panZoomInstance?.reset()
 
   return (
     <div className="mermaid-diagram" ref={parentRef}>
       {!printMode && additionalUI && (
         <MermaidControl
-          type={type}
+          type={diagramType}
           error={error}
           panZoom={panZoomMode}
           onReset={handleReset}
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react'
+import mermaid, { MermaidConfig, RenderResult } from 'mermaid'
+import SvgPanZoom from 'svg-pan-zoom'
+import { useState, useEffect, useRef } from 'react'
 
 export const useDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
@@ -33,4 +35,157 @@ export const useDarkMode = () => {
   }, [])
 
   return isDarkMode
+}
+
+export const useInkdropConfig = () => {
+  const [theme, setTheme] = useState<MermaidConfig['theme']>(
+    inkdrop.config.get('mermaid.theme') || 'default'
+  )
+  const [panZoomMode, setPanZoomMode] = useState<boolean>(
+    inkdrop.config.get('mermaid.panZoom')
+  )
+  const [additionalUI, setAdditionalUI] = useState<boolean>(
+    inkdrop.config.get('mermaid.additionalUI')
+  )
+
+  useEffect(() => {
+    const themeObserver = inkdrop.config.observe<MermaidConfig['theme']>(
+      'mermaid.theme',
+      setTheme
+    )
+    const panZoomObserver = inkdrop.config.observe<boolean>(
+      'mermaid.panZoom',
+      setPanZoomMode
+    )
+    const uiObserver = inkdrop.config.observe<boolean>(
+      'mermaid.additionalUI',
+      setAdditionalUI
+    )
+    return () => {
+      themeObserver.dispose()
+      panZoomObserver.dispose()
+      uiObserver.dispose()
+    }
+  }, [])
+
+  return { theme, panZoomMode, additionalUI }
+}
+
+const renderDiagram = async (
+  id: string,
+  code: string,
+  printMode: boolean
+): Promise<RenderResult> => {
+  mermaid.initialize({
+    startOnLoad: false,
+    theme: printMode ? 'default' : inkdrop.config.get('mermaid.theme'),
+    themeCSS: inkdrop.config.get('mermaid.themeCSS'),
+    themeVariables: JSON.parse(
+      inkdrop.config.get<string>('mermaid.themeVariables') || '{}'
+    )
+  })
+  try {
+    return await mermaid.render(id, code)
+  } catch (err) {
+    if (err instanceof Error) {
+      throw err
+    }
+    throw new Error('Unknown error')
+  }
+}
+
+export const useMermaidRendering = (
+  id: string,
+  code: string,
+  printMode: boolean,
+  panZoomMode: boolean,
+  additionalUI: boolean,
+  theme: MermaidConfig['theme']
+) => {
+  const [error, setError] = useState<Error | null>(null)
+  const [diagramType, setDiagramType] = useState<string>('')
+  const [panZoomInstance, setPanZoomInstance] = useState<ReturnType<
+    typeof SvgPanZoom
+  > | null>(null)
+
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const parentRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (!code || !containerRef.current) return
+    let cancelled = false
+    let resizeObserver: ResizeObserver | null = null
+    const container = containerRef.current
+    const parent = parentRef.current
+
+    renderDiagram(id, code, printMode)
+      .then(({ svg, diagramType, bindFunctions }) => {
+        if (cancelled || !svg.length) return
+
+        container.innerHTML = svg
+        const diagram = container.querySelector<SVGSVGElement>(`#${id}`)
+        if (!diagram) return
+
+        const viewBox = diagram.getAttribute('viewBox')
+        if (!viewBox) return
+        const [, , vbWidth, vbHeight] = viewBox
+          .trim()
+          .split(/[\s,]+/)
+          .map(parseFloat)
+        if ([vbWidth, vbHeight].some(isNaN)) return
+
+        diagram.setAttribute('height', '100%')
+        diagram.style.display = 'block'
+        diagram.style.maxWidth = '100%'
+
+        bindFunctions?.(container)
+
+        const recalcDimensions = () => {
+          const parentWidth = parent?.clientWidth || container.clientWidth
+          container.style.width = `${parentWidth}px`
+          container.style.height = `${Math.ceil(parentWidth * (vbHeight / vbWidth))}px`
+        }
+
+        recalcDimensions()
+        resizeObserver = new ResizeObserver(recalcDimensions)
+        resizeObserver.observe(container.parentElement!)
+
+        if (panZoomMode && !printMode && additionalUI) {
+          setPanZoomInstance(
+            SvgPanZoom(diagram, {
+              zoomEnabled: true,
+              mouseWheelZoomEnabled: false
+            })
+          )
+        }
+
+        setDiagramType(diagramType)
+        setError(null)
+      })
+      .catch(err => {
+        if (!cancelled) {
+          setError(err)
+        }
+      })
+
+    return () => {
+      cancelled = true
+      resizeObserver?.disconnect()
+    }
+  }, [id, code, theme, printMode, panZoomMode, additionalUI])
+
+  useEffect(() => {
+    if (!panZoomInstance || !containerRef.current) return
+    const resizeObserver = new ResizeObserver(() => {
+      panZoomInstance.resize()
+      panZoomInstance.center()
+      panZoomInstance.fit()
+    })
+    resizeObserver.observe(containerRef.current)
+    return () => {
+      resizeObserver.disconnect()
+    }
+  }, [panZoomInstance])
+
+  return { error, diagramType, containerRef, parentRef, panZoomInstance }
 }
